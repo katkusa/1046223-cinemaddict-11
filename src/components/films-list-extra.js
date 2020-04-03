76ca13a74c6049bd28b97d911cdc0d4ad785a3f4
ekaterina_kusa <kusa2706@mail.ru>
2020-04-03 18:47:35 +0300

    разбивает на модули и настравивает сервер

---
+++ src/components/films-list-extra.js
@@ -0,0 +1,5 @@
+export const createFilmsListExtra = (title) => (
+  `<section class="films-list--extra">
+    <h2 class="films-list__title">${title}</h2>
+    <div class="films-list__container"></div>
+  </section>`);
---
+++ src/components/statistics.js
@@ -0,0 +1,1 @@
+export const createStatistics = () => (`<p>130 291 movies inside</p>`);
